0e135be0a45e00c6a8ed652638d36eb926fd6a05
Moksh Jain <mokshjain222@gmail.com>
2020-04-01 19:49:37 +0530

    separated api module

--- index.js
+++ index.js
@@ -1,38 +1,9 @@
-//npm modules
-const axios = require('axios')
-const baseUrl = "http://localhost:3000"
+require('dotenv').config()
+var Queue = require('./lib/queue.js')
 
-function QuiddityAgent(queueName) {
-	this.queueName = queueName
-	this.agentId = "moksh1234"
-}
+var sq = new Queue('SimpleQueue')
 
-QuiddityAgent.prototype.execute = async function(cb) {
-	var obj = this
-	try {
-		let message = await this.fetchMessage()
-		cb(message, function() {
-
-			axios.get(`${baseUrl}/queue/delete?queueName=${obj.queueName}&agentId=${obj.agentId}&_id=${message._id}`)
-			.then(res => {
-				console.log(res.data)
-			})
-			.catch(err => console.log(err))
-		})
-
-	} catch (err) {
-		console.log(err)
-	}
-
-}
-
-QuiddityAgent.prototype.fetchMessage = async function() {
-	let res = await axios.get(`${baseUrl}/queue/fetch?queueName=${this.queueName}&agentId=${this.agentId}`)
-	return res.data
-}
-
-let agent = new QuiddityAgent('SimpleQueue')
-agent.execute((message, done) => {
+sq.execute((message, done) => {
 	console.log(message)
 	done()
 })
