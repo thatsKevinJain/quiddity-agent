cc3e932940323607642dda4dc2f0caa6a5d70c35
Kevin <thatskevinjain@gmail.com>
2020-04-08 22:39:32 +0530

    Added comments and env's

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
-// export agent
-var Queue = require('./lib/queue.js')
 
-var fifo = new Queue("fifo", { 
-	where: {
-		email: "moksh@gmail.com"
-	} 
-})	
-
-fifo.pull((message, done) => {
-	console.log(message)
-	done()
-})
-
-module.exports = Queue
+module.exports = require('./lib/queue.js')
